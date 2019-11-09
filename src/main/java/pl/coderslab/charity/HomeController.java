dda8c2276ead8237bbaa5970399b45cfd0cebd7f
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-11-10 00:06:21 +0100

    sum of all donations

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,6 +3,7 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 
@@ -10,14 +11,17 @@ import pl.coderslab.charity.repository.InstitutionRepository;
 public class HomeController {
 
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("donationQuantity", donationRepository.getQuantity());
         return "index";
     }
 }
