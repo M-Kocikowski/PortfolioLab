32bf454ded3fa63f02bddb2fcb7aea126153dd11
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-11-14 23:04:02 +0100

    #3 - donation controller - endpoints instead of model attributes for js operations

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,7 +3,8 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.Institution;
@@ -13,6 +14,7 @@ import pl.coderslab.charity.repository.InstitutionRepository;
 import java.util.List;
 
 @Controller
+@RequestMapping("/donation")
 public class DonationController {
 
     private InstitutionRepository institutionRepository;
@@ -23,21 +25,22 @@ public class DonationController {
         this.categoryRepository = categoryRepository;
     }
 
-    @GetMapping("/donation")
+    @GetMapping()
     public String donationAction(Model model){
         model.addAttribute("donation", new Donation());
         return "form";
     }
 
-    @ModelAttribute("institutions")
-    public List<Institution> getInstitutions(){
-        return institutionRepository.findAll();
-    }
-
-    @ModelAttribute("categories")
-    public List<Category> getCategories(){
+    @GetMapping("/categories")
+    @ResponseBody
+    public List<Category> getAllCategories(){
         return categoryRepository.findAll();
     }
 
+    @GetMapping("/institutions")
+    @ResponseBody
+    public List<Institution> getAllInstitutions(){
+        return institutionRepository.findAll();
+    }
 
 }
